4d04c80663efbc2936828fe759d81dce7615d207
USERCOMP500\fchepiga <fchepiga@playflock.com>
2017-06-16 13:30:13 +0300

    Add RectangleView

---
+++ Assets/RectangleView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectangleView : MonoBehaviour {
+   public SpriteRenderer renderer;
+    
+
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.color = Random.ColorHSV();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            renderer.color =Random.ColorHSV();
+        }
+	}
+
+}
